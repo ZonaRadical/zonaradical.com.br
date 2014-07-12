ac2c3071b7dcb5d361400585b622ce01a20b213b
Nikita Zonov <nzonov@gmail.com>
2014-07-12 20:04:25 +0200

    Remove /vendor/bundle as Heroku advised

--- vendor/bundle/gems/i18n-tasks-0.5.4/lib/i18n/tasks/locale_pathname.rb
+++
@@ -1,9 +0,0 @@
-module I18n::Tasks
-  module LocalePathname
-    extend self
-
-    def replace_locale(path, from, to)
-      path.try :sub, /(^|[\/.])#{from}(?=\.)/, "\\1#{to}"
-    end
-  end
-end
--- vendor/bundle/gems/i18n-tasks-0.5.4/lib/i18n/tasks/version.rb
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-module I18n
-  module Tasks
-    VERSION = '0.5.4'
-  end
-end
