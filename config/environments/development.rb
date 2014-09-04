77205d1db399c0ef371a909eeda2c7b1f1bde505
Nikita Zonov <nzonov@gmail.com>
2014-09-04 22:32:10 +0400

    Установил различные локали для Production и Development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
+  config.i18n.default_locale = :en
 end
