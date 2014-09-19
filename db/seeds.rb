b4d7c36db73699fd1ed23cfa92387d87fb0eec21
Nikita Zonov <nzonov@gmail.com>
2014-09-19 16:00:50 +0400

    Добавили роли в seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# добавляем роли
+Role.create(:name => 'admin')
+Role.create(:name => 'editor')
