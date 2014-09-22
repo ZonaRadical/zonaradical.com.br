170033af6a3f244855dbd49f9aab81aa4354b480
Nikita Zonov <nzonov@gmail.com>
2014-09-22 22:06:52 +0400

    Добавил аватар и включил ability

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,5 +1,5 @@
-Forem.user_class = "User"
-Forem.email_from_address = "please-change-me@example.com"
+Forem.user_class = 'User'
+Forem.email_from_address = "contact@zonaradical.com.br"
 # If you do not want to use gravatar for avatars then specify the method to use here:
 # Forem.avatar_user_method = :custom_avatar_url
 Forem.per_page = 20
@@ -9,11 +9,10 @@ Forem.per_page = 20
 
 # Rails.application.config.to_prepare do
 #   If you want to change the layout that Forem uses, uncomment and customize the next line:
-
     Forem::ApplicationController.layout 'forem'
-
+    Forem.avatar_user_method = :avatar_url
 #   If you want to add your own cancan Abilities to Forem, uncomment and customize the next line:
-#   Forem::Ability.register_ability(Ability)
+    Forem::Ability.register_ability(Ability)
 # end
 #
 # By default, these lines will use the layout located at app/views/layouts/forem.html.erb in your application.
