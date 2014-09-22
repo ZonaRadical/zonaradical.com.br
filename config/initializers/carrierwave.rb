109faa29e8481fcc13f06c23496e7a3fd0acb9ec
Nikita Zonov <nzonov@gmail.com>
2014-09-22 20:08:08 +0400

    Вместо NullStorage  в тестовом окружении не будем процессить файлы

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,27 +1,6 @@
 # NullStorage provider for CarrierWave for use in tests.  Doesn't actually
 # upload or store files but allows test to pass as if files were stored and
 # the use of fixtures.
-class NullStorage
-  attr_reader :uploader
-
-  def initialize(uploader)
-    @uploader = uploader
-  end
-
-  def identifier
-    uploader.filename
-  end
-
-  def store!(_file)
-    true
-  end
-
-  def retrieve!(_identifier)
-    true
-  end
-end
-
-
 CarrierWave.configure do |config|
   if Rails.env == 'production'
     config.storage :fog
@@ -35,7 +14,7 @@ CarrierWave.configure do |config|
     }
     config.fog_directory  = 'zonaradical'                     # required
   elsif Rails.env == 'test'
-    config.storage 'NullStorage'
+    config.storage :file
     # Required to prevent FactoryGirl from giving an infuriating exception
     # ArgumentError: wrong exec option
     # It also speeds up tests so it's a good idea
