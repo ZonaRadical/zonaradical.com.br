349fe67de51b7fd08437fa9e3c4bffbfbf1064a1
Nikita Zonov <nzonov@gmail.com>
2014-09-26 12:31:48 +0400

    Администратор умеет назначать роли

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+describe 'Users', :type => :request do
+
+  subject { page }
+
+
+  describe 'Logged in as admin' do
+    before(:each) do
+      @admin = FactoryGirl.create(:admin)
+      login_as @admin, :scope => :user
+      visit user_path(@admin.id)
+    end
+
+
+    it { should have_css('h1', :text => 'Editing profile') }
+    it { should have_content(I18n.t('assignedRoles')) }
+  end
+end
