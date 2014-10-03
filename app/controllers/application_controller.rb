48eb2840b3cc1811f97274ad5503270e07efd3d4
Nikita Zonov <nzonov@gmail.com>
2014-10-03 22:05:17 +0400

    Сделал динамическое меню до 3го уровня, если надо дальше логика понятна в файле resort_category_helper.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_locale
+  before_action :set_locale, :set_menu
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -26,5 +26,8 @@ class ApplicationController < ActionController::Base
   end
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
+  end
+  def set_menu
+    @resort_categories = ResortCategory.all
   end
 end
--- app/helpers/resort_categories_helper.rb
+++ app/helpers/resort_categories_helper.rb
@@ -1,2 +1,23 @@
 module ResortCategoriesHelper
+  def nested_resort_categories(resort_categories)
+    resort_categories.map do |resort_category, sub_resort_category|
+      render(resort_category) + content_tag(:ul, nested_resort_categories(sub_resort_category), :class=> 'sub_resort_category')
+    end.join.html_safe
+  end
+
+  def menu_resort_categories(resort_categories)
+    resort_categories.map do |resort_category, sub_resort_category|
+      content_tag(:li, link_to(resort_category.name, resort_category_path(resort_category.id)) + content_tag(:ul, sub_menu_resort_categories(sub_resort_category),:class => 'subsubmenu'),:class => 'subsubmenu-item' )
+    end.join.html_safe
+  end
+
+  def sub_menu_resort_categories(resort_categories)
+    resort_categories.map do |resort_category, sub_resort_category|
+      content_tag(:li,link_to(resort_category.name, resort_category_path(resort_category.id)))
+    end.join.html_safe
+  end
+
+
 end
+
+
