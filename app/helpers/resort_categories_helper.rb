e404ab5c664db833c364e0c2261902110fb27490
Nikita Zonov <nzonov@gmail.com>
2014-10-04 03:44:22 +0400

    Ссылка не работала на странице форума

--- app/helpers/resort_categories_helper.rb
+++ app/helpers/resort_categories_helper.rb
@@ -7,7 +7,7 @@ module ResortCategoriesHelper
 
   def menu_resort_categories(resort_categories)
     resort_categories.map do |resort_category, sub_resort_category|
-      content_tag(:li, link_to(resort_category.name, resort_category_path(resort_category.id)) + content_tag(:ul, sub_menu_resort_categories(sub_resort_category),:class => 'subsubmenu'),:class => 'subsubmenu-item' )
+      content_tag(:li, link_to(resort_category.name, main_app.resort_category_path(resort_category.id)) + content_tag(:ul, sub_menu_resort_categories(sub_resort_category),:class => 'subsubmenu'),:class => 'subsubmenu-item' )
     end.join.html_safe
   end
 
