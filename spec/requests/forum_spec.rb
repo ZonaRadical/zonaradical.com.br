c05ca6b81e2fd194f899c094736cc8f744cc3dde
Nikita Zonov <nzonov@gmail.com>
2014-10-04 04:16:02 +0400

    Добавил тесты на главную страницу форума

---
+++ spec/requests/forum_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+describe 'Forum', :type => :request do
+
+  subject { page }
+
+  describe 'Logged in as User' do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      login_as @user, :scope => :user
+      visit forem_path
+    end
+
+
+    it { should have_css('div#forums_container') }
+    it { should have_css('h1',:text => I18n.t('forem.forums.index.title')) }
+    it { should_not have_link(I18n.t("area", :scope => 'forem.admin'),forem.admin_root_path) }
+  end
+
+  describe 'Logged in as admin' do
+    before(:each) do
+      @admin = FactoryGirl.create(:admin)
+      @admin.forem_admin = true
+      @admin.save
+      login_as @admin, :scope => :user
+      visit forem_path
+    end
+
+
+    it { should have_css('div#forums_container') }
+    it { should have_css('h1',:text => I18n.t('forem.forums.index.title')) }
+    it { should have_link(I18n.t("area", :scope => 'forem.admin'),forem.admin_root_path) }
+  end
+end
