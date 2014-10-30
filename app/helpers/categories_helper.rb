8e46b8fded52e6bd6992b0c22cb79662041f07b1
Nikita Zonov <nzonov@gmail.com>
2014-10-31 01:31:52 +0300

    Добавил динамическое меню

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -7,13 +7,13 @@ module CategoriesHelper
 
   def menu_categories(categories)
     categories.map do |category, sub_category|
-      content_tag(:li, link_to(category.name, main_app.category_path(category.id)) + content_tag(:ul, sub_menu_categories(sub_category),:class => 'subsubmenu'),:class => 'subsubmenu-item' )
+      content_tag(:li, link_to(category.name, url_for(category)) + content_tag(:ul, sub_menu_categories(sub_category),:class => 'subsubmenu'),:class => 'subsubmenu-item' )
     end.join.html_safe
   end
 
   def sub_menu_categories(categories)
-    categories.map do |category, sub_category|
-      content_tag(:li,link_to(category.name, main_app.category_path(category.id)))
+    categories.map do |category|
+      content_tag(:li,link_to(url_for(category)))
     end.join.html_safe
   end
 
---
+++ app/helpers/tip_categories_helper.rb
@@ -0,0 +1,23 @@
+# @todo Перенсти это все в один хэлпер для категорий
+module TipCategoriesHelper
+  def nested_tip_categories(tip_categories)
+    tip_categories.map do |tip_category, sub_tip_category|
+      render(tip_category) + content_tag(:ul, nested_tip_categories(sub_tip_category), :class=> 'sub_tip_category')
+    end.join.html_safe
+  end
+
+  def menu_tip_categories(tip_categories)
+    tip_categories.map do |tip_category, sub_tip_category|
+      content_tag(:li, link_to(tip_category.name, main_app.tip_category_path(tip_category.id)) + content_tag(:ul, sub_menu_tip_categories(sub_tip_category),:class => 'subsubmenu'),:class => 'subsubmenu-item' )
+    end.join.html_safe
+  end
+
+  def sub_menu_tip_categories(tip_categories)
+    tip_categories.map do |tip_category, sub_tip_category|
+      content_tag(:li,link_to(tip_category.name, main_app.tip_category_path(tip_category.id)))
+    end.join.html_safe
+  end
+
+end
+
+
