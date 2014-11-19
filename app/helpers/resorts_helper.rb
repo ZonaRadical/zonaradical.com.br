553fbebfaf84a26ba6de07d12b2805aa4e6c8ab1
Ignat <ifialk@gmail.com>
2014-11-19 16:07:15 -0200

    resort designs

--- app/helpers/resorts_helper.rb
+++ app/helpers/resorts_helper.rb
@@ -1,2 +1,25 @@
 module ResortsHelper
+
+  def print_pic_base(resort_data_pic,resort_data_base,resort_units)
+    if resort_data_pic.to_s.empty? or resort_data_base.to_s.empty?
+      descr=''
+    else
+      descr=resort_data_pic.to_s
+      descr+=' - '
+      descr+=resort_data_base.to_s
+      descr+=' '
+      descr+=resort_units.to_s
+    end
+  end
+
+  def print_with_meters(resort_data, resort_units)
+    if resort_data.to_s.empty?
+      descr=''
+    else
+      descr=resort_data.to_s
+      descr+=' '
+      descr+=resort_units.to_s
+    end
+  end
+
 end
