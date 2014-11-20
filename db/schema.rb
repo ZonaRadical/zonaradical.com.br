b56fc8b0f4ccfe497c1ca1484b19f93269ebf810
Nikita Zonov <nzonov@gmail.com>
2014-11-20 14:03:11 +0300

    After Gem Forem update we have to import Forem Migration to the project
      https://github.com/radar/forem/wiki/Installing---Upgrading

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118153839) do
+ActiveRecord::Schema.define(version: 20141120105941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,10 +40,11 @@ ActiveRecord::Schema.define(version: 20141118153839) do
   end
 
   create_table "forem_categories", force: true do |t|
-    t.string   "name",       null: false
+    t.string   "name",                   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
+    t.integer  "position",   default: 0
   end
 
   add_index "forem_categories", ["slug"], name: "index_forem_categories_on_slug", unique: true, using: :btree
@@ -54,6 +55,7 @@ ActiveRecord::Schema.define(version: 20141118153839) do
     t.integer "category_id"
     t.integer "views_count", default: 0
     t.string  "slug"
+    t.integer "position",    default: 0
   end
 
   add_index "forem_forums", ["slug"], name: "index_forem_forums_on_slug", unique: true, using: :btree
