f8f58ebfe1bee8999798f44f97ec98526d33fa76
Nikita Zonov <nzonov@gmail.com>
2014-11-25 22:10:00 +0300

    Added to resort alternative version of the WYSIWYG

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,18 @@
 //= require inclusions
 
 
+//= require froala_editor.min.js
+//= require plugins/block_styles.min.js
+// = require plugins/colors.min.js
+// = require plugins/media_manager.min.js
+// = require plugins/tables.min.js
+// = require plugins/video.min.js
+// = require plugins/font_family.min.js
+// = require plugins/font_size.min.js
+// = require plugins/file_upload.min.js
+// = require plugins/lists.min.js
+// = require plugins/char_counter.min.js
+
 /* Запускаем когда страница готова | trigger when page is ready */
 $(document).ready(function(){
 
