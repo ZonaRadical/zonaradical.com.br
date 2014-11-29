0efb7437d501edffbac34d491f51bdad0421b9ff
Nikita Zonov <nzonov@gmail.com>
2014-11-30 02:21:24 +0300

    Allow tips without description

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -13,7 +13,6 @@
 class Tip < ActiveRecord::Base
 
   validates :title, presence: true
-  validates :level1_description, presence: true
   validates :tip_category_id, presence: true
 
   belongs_to :tip_category
