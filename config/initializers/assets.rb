5ca01b67997a1e42f7a7fe02609bef66111b3ba8
Nikita Zonov <nzonov@gmail.com>
2014-11-29 13:09:50 +0300

    Changed cause of https://github.com/activeadmin/activeadmin/issues/3072
    Note that this is generally a bad idea. Most of the time you shouldn't be globbing all possible assets for precompilation. The Rails convention is to concatenate them together to limit unnecessary HTTP requests.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,25 @@
 
-Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
-
+# forem
 Rails.application.config.assets.precompile += %w( forem.css )
 Rails.application.config.assets.precompile += %w( forem.js )
+
+
+# controllers
+
+Rails.application.config.assets.precompile += %w( static_pages.css )
+Rails.application.config.assets.precompile += %w( static_pages.js )
+
+Rails.application.config.assets.precompile += %w( users.css )
+Rails.application.config.assets.precompile += %w( users.js )
+
+Rails.application.config.assets.precompile += %w( resorts.css )
+Rails.application.config.assets.precompile += %w( resorts.js )
+
+Rails.application.config.assets.precompile += %w( resort_categories.css )
+Rails.application.config.assets.precompile += %w( resort_categories.js )
+
+Rails.application.config.assets.precompile += %w( tips.css )
+Rails.application.config.assets.precompile += %w( tips.js )
+
+Rails.application.config.assets.precompile += %w( tip_categories.css )
+Rails.application.config.assets.precompile += %w( tip_categories.js )
