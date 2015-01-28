4283af099a52d659c20a6ba2df55f35697315557
ignat <ifialk@gmail.com>
2015-01-28 17:31:36 -0200

    fixing videos and categories' urls

--- app/helpers/video_categories_helper.rb
+++ app/helpers/video_categories_helper.rb
@@ -3,16 +3,16 @@ module VideoCategoriesHelper
   def menu_video_categories(video_categories)
     video_categories.map do |video_categories, sub_video_categories|
       if sub_video_categories.empty?
-        content_tag(:li, link_to(video_categories.name, main_app.video_categories_path(video_categories.id)))
+        content_tag(:li, link_to(video_categories.name, "../videos"))
       else
-        content_tag(:li, link_to(video_categories.name, main_app.video_categories_path(video_categories.id)) + content_tag(:ul, sub_menu_video_categories(sub_video_categories),:class => 'subsubmenu'),:class => 'subsubmenu-item' )
+        content_tag(:li, link_to(video_categories.name, "../videos") + content_tag(:ul, sub_menu_video_categories(sub_video_categories),:class => 'subsubmenu'),:class => 'subsubmenu-item' )
       end
 
     end.join.html_safe
   end
   def sub_menu_video_categories(video_categories)
     video_categories.map do |video_categories, sub_video_category|
-      content_tag(:li,link_to(video_categories.name, main_app.video_categories_path(video_categories.id)))
+      content_tag(:li,link_to(video_categories.name, "../videos"))
     end.join.html_safe
   end
 end
