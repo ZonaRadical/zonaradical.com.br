432868deef1fd5b069cf303849097bd21f8d9253
Nikita Zonov <joomalungma@gmail.com>
2015-02-10 21:17:24 +0300

    Changed Forem.email_from_address = 'noreply@zonaradical.com.br'

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,5 +1,5 @@
 Forem.user_class = 'User'
-Forem.email_from_address = "contact@zonaradical.com.br"
+Forem.email_from_address = 'noreply@zonaradical.com.br'
 # If you do not want to use gravatar for avatars then specify the method to use here:
 # Forem.avatar_user_method = :custom_avatar_url
 Forem.per_page = 20
