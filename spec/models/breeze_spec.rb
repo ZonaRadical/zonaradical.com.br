82219df3e9f304eb89fc88bf4f3c05e64ddb4bea
Thiago Ribeiro <thiagitosouza@hotmail.com>
2015-02-27 14:09:28 -0300

    Add tests for models in spec/models folder
    Signed-off-by: Thiago Ribeiro <thiagitosouza@hotmail.com>

--- spec/models/breeze_spec.rb
+++ spec/models/breeze_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Breeze, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "Can be instantiated" do
+    expect(Breeze.new).to be_an_instance_of(Breeze)
+  end
+
+  it { should belong_to(:breeze_category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:breeze_category_id) }
+
 end
