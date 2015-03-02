8bc27f47282469a48e2500b2eae45c1ef813904d
Carlos Eduardo Rodrigues Diógenes <carlos.diogenes84@gmail.com>
2015-03-01 21:30:10 -0300

    avoid undefined method when the implicit route is not main_app

--- app/helpers/video_categories_helper.rb
+++ app/helpers/video_categories_helper.rb
@@ -3,16 +3,16 @@ module VideoCategoriesHelper
   def menu_video_categories(video_categories)
     video_categories.map do |video_category, sub_video_categories|
       if sub_video_categories.empty?
-        content_tag(:li, link_to(video_category.name, video_category_path(video_category)))
+        content_tag(:li, link_to(video_category.name, main_app.video_category_path(video_category)))
       else
-        content_tag(:li, link_to(video_category.name, video_category_path(video_category)) + content_tag(:ul, sub_menu_video_categories(sub_video_categories),:class => 'subsubmenu'),:class => 'subsubmenu-item')
+        content_tag(:li, link_to(video_category.name, main_app.video_category_path(video_category)) + content_tag(:ul, sub_menu_video_categories(sub_video_categories),:class => 'subsubmenu'),:class => 'subsubmenu-item')
       end
 
     end.join.html_safe
   end
   def sub_menu_video_categories(video_categories)
     video_categories.map do |video_category, sub_video_category|
-      content_tag(:li,link_to(video_category.name, video_category_path(video_category)))
+      content_tag(:li,link_to(video_category.name, main_app.video_category_path(video_category)))
     end.join.html_safe
   end
 end
