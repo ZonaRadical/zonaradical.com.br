430796d03384084b9678dd40c1386a11ceeb91c0
Carlos Eduardo Rodrigues Diógenes <carlos.diogenes@fnix.com.br>
2015-03-12 17:58:11 -0300

    refined active menu requirements

--- spec/features/active_menu_spec.rb
+++ spec/features/active_menu_spec.rb
@@ -2,37 +2,62 @@ require 'rails_helper'
 
 feature 'active menu' do
   scenario 'main menu of current page is highlighted' do
+    create(:video_category, name: 'Digiácomo Dias')
+    create(:resort_category, name: 'North')
+    create(:tip_category, name: 'Id Odit Molestiae')
+    create(:forem_category, name: 'Travels')
+
     visit root_path
     
+    within 'ul.menu' do click_link 'Home' end
     within 'ul.menu' do
-      click_link 'Home'
+      expect(page).to have_selector('a.active', text: 'Home')
+      expect(page).to have_selector('a.active', count: 1)
     end
-    expect(page).to have_selector('a.active', text: 'Home')
-    expect(page).to have_selector('a.active', count: 1)
-    
+    within 'ul.menu' do click_link 'Digiácomo Dias' end
     within 'ul.menu' do
-      click_link 'Resorts'
+      expect(page).to have_selector('a.active', text: 'Home')
+      expect(page).to have_selector('a.active', count: 1)
     end
-    expect(page).to have_selector('a.active', text: 'Resorts')
-    expect(page).to have_selector('a.active', count: 1)
-    
+
+    within 'ul.menu' do click_link 'Resorts' end
     within 'ul.menu' do
-      click_link 'Tips'
+      expect(page).to have_selector('a.active', text: 'Resorts')
+      expect(page).to have_selector('a.active', count: 1)
     end
-    expect(page).to have_selector('a.active', text: 'Tips')
-    expect(page).to have_selector('a.active', count: 1)
+    within 'ul.menu' do click_link 'North' end
+    within 'ul.menu' do
+      expect(page).to have_selector('a.active', text: 'Resorts')
+      expect(page).to have_selector('a.active', count: 1)
+    end
     
+    within 'ul.menu' do click_link 'Tips' end
+    within 'ul.menu' do
+      expect(page).to have_selector('a.active', text: 'Tips')
+      expect(page).to have_selector('a.active', count: 1)
+    end
+    within 'ul.menu' do click_link 'Id Odit Molestiae' end
+    within 'ul.menu' do
+      expect(page).to have_selector('a.active', text: 'Tips')
+      expect(page).to have_selector('a.active', count: 1)
+    end
+
     # within 'ul.menu' do
     #   click_link 'Vamo junto'
     # end
     # expect(page).to have_selector('a.active', text: 'Vamo junto')
     # expect(page).to have_selector('a.active', count: 1)
     
+    within 'ul.menu' do click_link 'Forum' end
     within 'ul.menu' do
-      click_link 'Forum'
+      expect(page).to have_selector('a.active', text: 'Forum')
+      expect(page).to have_selector('a.active', count: 1)
     end
-    expect(page).to have_selector('a.active', text: 'Forum')
-    expect(page).to have_selector('a.active', count: 1)
+    within 'ul.menu' do click_link 'Travels' end
+    within 'ul.menu' do
+      expect(page).to have_selector('a.active', text: 'Forum')
+      expect(page).to have_selector('a.active', count: 1)
+    end
     
     # within 'ul.menu' do
     #   click_link 'Galera ZR'
