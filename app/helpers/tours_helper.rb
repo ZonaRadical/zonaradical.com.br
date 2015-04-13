9220e3fbc76ffc239c4f75c98d86cded8f60b5c0
Carlos Diógenes <carlos.diogenes@fnix.com.br>
2015-04-13 10:13:50 -0300

    concat must be used to output all form fields

--- app/helpers/tours_helper.rb
+++ app/helpers/tours_helper.rb
@@ -1,9 +1,9 @@
 module ToursHelper
   def participate_button(tour)
     form_for [tour, Tour::Participant.new] do |f|
-      f.hidden_field :tour_id, value: tour.id
-      f.hidden_field :user_id, value: current_user.id
-      f.submit t('participate')
+      concat(f.hidden_field :tour_id, value: tour.id)
+      concat(f.hidden_field :user_id, value: current_user.id)
+      concat(f.submit t('participate'))
     end unless tour.user_owners.include?(current_user)
   end
 end
