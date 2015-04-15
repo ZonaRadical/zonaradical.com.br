4d0ba96b2afedfc7f1c5009f5467151c59e53747
Carlos Diógenes <carlos.diogenes@fnix.com.br>
2015-04-14 21:38:46 -0300

    public tour views must reference only public tour actions

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -3,10 +3,18 @@ class ToursController < ApplicationController
     load_tours
   end
 
+  def show
+    load_tour
+  end
+
   private
 
   def load_tours
     @tours ||= tour_scope.order(created_at: :desc).to_a
+  end
+
+  def load_tour
+    @tour ||= tour_scope.find(params[:id])
   end
 
   def tour_scope
