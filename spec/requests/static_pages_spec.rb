8d11686e198214665e5110b9fa2e990218f13b0f
Ignat <ifialk@gmail.com>
2015-04-25 08:44:16 -0300

    fixing tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,6 @@ describe 'StaticPages', :type => :request do
   describe 'Home' do
     before { visit root_path }
 
-    it { should have_link( I18n.t('forum') ) }
     it { should have_link( I18n.t('resorts') ) }
     it { should have_link( I18n.t('home') ) }
     it { should have_content( I18n.t('signIn') ) }
