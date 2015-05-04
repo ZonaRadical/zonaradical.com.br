63e681bbcd02d79ad0c38e094bdca775da3be5eb
Carlos Diógenes <carlos.diogenes@fnix.com.br>
2015-05-04 20:29:35 -0300

    Ouput owner pic and resort logo in tour/offer show view.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
 	def user_last_seen_at(user = @user)
 		user.last_seen_at.nil? ? t('never') : distance_of_time_in_words_to_now(user.last_seen_at)
 	end
+
+  def avatar_img(user)
+    image_tag(user.avatar_url, class: 'userpic', alt: user.name)
+  end
 end
