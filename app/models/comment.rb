2690c16f50d0a3d87a1fadd71852459a06c1f4b7
Carlos Diógenes <carlos.diogenes@fnix.com.br>
2015-05-15 15:56:25 -0300

    the topic is already in discourse when post a comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,23 +1,14 @@
 class Comment
   include ActiveModel::Model
-  
+
+  attr_accessor :tour_id
+  attr_accessor :surname  
   attr_accessor :content
 
-  def save(tour, user)
-    create_topic(tour, user.surname) if tour.discourse_topic_id.nil?
-  end
-
-  private
-
-  def create_topic(tour, api_username)
+  def save
+    tour = Tour.find(tour_id)
     client = DiscourseZr.client
-    client.api_username = api_username
-    topic = client.create_topic(
-      title: tour.title,
-      raw: content,
-      category: tour.resort_categories.first.name.downcase,
-      skip_validations: true
-    )
-    tour.update_attribute(:discourse_topic_id, topic['id'])
+    client.api_username = surname
+    client.create_post(topic_id: tour.discourse_topic_id, raw: content)
   end
 end
