a00e25eda6e4b65705d79cb809fd128a375bdcf0
Bruno Zeraik <brunozrk@gmail.com>
2015-05-17 21:00:00 -0300

    add shared example in tips_controller

--- spec/controllers/tips_controller_spec.rb
+++ spec/controllers/tips_controller_spec.rb
@@ -173,15 +173,7 @@ RSpec.describe TipsController, :type => :controller do
       end
     end
 
-    describe 'GET #slug' do
-      let(:slug_params) { { title: 'test slug' } }
-      let(:response_expected) { { slug: 'test-slug' } }
-
-      it 'returns a json with slug preview' do
-        get :slug, slug_params
-        expect(response.body).to eq response_expected.to_json
-      end
-    end
+    it_behaves_like 'slugs_preview'
   end
 
   describe 'Not Admin' do
