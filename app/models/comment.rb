c20414a252c1bb3f2bb3e75830b1814f345f0da3
Carlos Diógenes <carlos.diogenes@fnix.com.br>
2015-05-19 15:48:43 -0300

    Integration is now being made with ZR user id and Discourse external id.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,13 @@ class Comment
   include ActiveModel::Model
 
   attr_accessor :tour_id
-  attr_accessor :surname  
+  attr_accessor :user_id
   attr_accessor :content
 
   def save
     tour = Tour.find(tour_id)
-    client = DiscourseZr.client
-    client.api_username = surname
+    discourse_user = DiscourseZr.client.user_by_external_id(user_id)
+    client = DiscourseZr.client(username: discourse_user['username'])
     client.create_post(topic_id: tour.discourse_topic_id, raw: content)
   end
 end
