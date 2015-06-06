e310a1aa270f9f25264317d49af3abf4623f6e57
Carlos Diógenes <carlos.diogenes@fnix.com.br>
2015-06-06 16:24:44 -0300

    Links between ZR tour and it's corresponding Discourse topic.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
   config.i18n.default_locale = 'pt-BR'
+
+  routes.default_url_options = { :host => "zonaradical.dev:3000" }
 end
