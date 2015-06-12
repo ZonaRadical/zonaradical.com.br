29e3c60abb2919d362f0de4efe283daf324e05df
Nikita Zonov <joomalungma@gmail.com>
2015-06-13 02:02:20 +0300

    Move to the single file and Fix User validation

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,26 +1,29 @@
 FactoryGirl.define do
   factory :user do
     name     'Test user'
-    surname  'Surname'
+    surname  'TestUser'
     email    'mail@mail123.ru'
     password 'foobar'
     password_confirmation 'foobar'
     confirmed_at Time.now
 
     factory :admin do
-      name 'Admin'
+      name 'AdminUser'
+      surname 'AdminUser'
       email 'admin@mail.com'
       after(:create){|u| u.roles << create(:role, name: 'admin')}
     end
 
     factory :editor do
-      name 'Editor'
+      name 'EditorUser'
+      surname 'EditorUser'
       email 'editor@mail.com'
       after(:create){|u| u.roles << create(:role, name: 'editor')}
     end
 
     factory :athlete do
-      name 'Athlete'
+      name 'AthleteUser'
+      surname 'AthleteUser'
       email 'athlete@mail.com'
       after(:create) { |u| u.roles << create(:role, name: 'athlete') }
     end
