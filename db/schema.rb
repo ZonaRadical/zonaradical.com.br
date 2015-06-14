120f66039d17d262a867bae1e0db6874fcdc7021
Bruno Zeraik <brunozrk@gmail.com>
2015-06-14 16:03:25 -0300

    add column slug to video categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609001036) do
+ActiveRecord::Schema.define(version: 20150614190158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -435,6 +435,7 @@ ActiveRecord::Schema.define(version: 20150609001036) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+    t.string   "slug"
   end
 
   create_table "videos", force: true do |t|
