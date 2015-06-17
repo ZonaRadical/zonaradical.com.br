983dcf8710a0c9bd71751656615a626cd45172b2
Carlos Diógenes <carlos.diogenes@fnix.com.br>
2015-06-17 20:07:52 -0300

    Show only resorts from HSul in root page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def index
     # @todo Add caching of the data, for large tables
     # performance of the RANDOM order in DB is bad
-    @resort = Resort.order('RANDOM()').first
+    @resort = Resort.joins(:resort_category).where(resort_categories: { ancestry: '1' }).order('RANDOM()').first
 
     @tip = Tip.order('RANDOM()').first
     @tipsOrdered = Tip.order(created_at: :desc)[0..2]
